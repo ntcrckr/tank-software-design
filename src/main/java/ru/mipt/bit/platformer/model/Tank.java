a63ba8d42e7db08078a108d093a59dc31cc22fc0
Alexei Pozdnyakov <alexei.pozdnyakov@mail.ru>
2023-10-09 22:05:47 +0300

    HW03 test fixes wip

--- src/main/java/ru/mipt/bit/platformer/model/Tank.java
+++ src/main/java/ru/mipt/bit/platformer/model/Tank.java
@@ -1,6 +1,9 @@
 package ru.mipt.bit.platformer.model;
 
+import org.lwjgl.system.NonnullDefault;
 import ru.mipt.bit.platformer.controller.MoveAction;
+
+import java.util.Objects;
 
 import static com.badlogic.gdx.math.MathUtils.isEqual;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.continueProgress;
@@ -64,10 +67,23 @@ public class Tank implements GameObject, Controllable {
     }
 
     @Override
-    public Controllable afterAction(MoveAction moveAction) {
+    public Controllable afterAction(@NonnullDefault MoveAction moveAction) {
         if (isMoving()) {
-            return new Tank(coordinates, direction, movementSpeed);
+            return this;
         }
         return new Tank(moveAction.getDirection().apply(coordinates), direction, movementSpeed);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tank tank = (Tank) o;
+        return Float.compare(movementProgress, tank.movementProgress) == 0 && Float.compare(movementSpeed, tank.movementSpeed) == 0 && Objects.equals(coordinates, tank.coordinates) && Objects.equals(destinationCoordinates, tank.destinationCoordinates) && direction == tank.direction;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinates, destinationCoordinates, movementProgress, direction, movementSpeed);
     }
 }
--- src/test/java/ru/mipt/bit/platformer/model/TankTest.java
+++ src/test/java/ru/mipt/bit/platformer/model/TankTest.java
@@ -1,11 +1,69 @@
 package ru.mipt.bit.platformer.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import ru.mipt.bit.platformer.controller.MoveAction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TankTest {
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testTankMovingToCoordinatesOnMovement(Direction direction) {
+        Tank tank = new Tank(new Coordinates(0, 0), Direction.RIGHT, 1f);
+        tank.startMovement(direction);
+        Coordinates tankDestinationCoordinates = tank.getDestinationCoordinates();
+        Coordinates tankExpectedDestinationCoordinates = direction.apply(tank.getCoordinates());
+        assertEquals(tankDestinationCoordinates, tankExpectedDestinationCoordinates);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testTankChangeDirectionOnMovement(Direction direction) {
+        Tank tank = new Tank(new Coordinates(0, 0), Direction.RIGHT, 1f);
+        tank.startMovement(direction);
+        float tankRotation = tank.getRotation();
+        float tankExpectedRotation = direction.getRotation();
+        assertEquals(tankRotation, tankExpectedRotation);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testTankIsMovingOnMovement(Direction direction) {
+        Tank tank = new Tank(new Coordinates(0, 0), Direction.RIGHT, 1f);
+        tank.startMovement(direction);
+        boolean tankIsMoving = tank.isMoving();
+        boolean tankExpectedIsMoving = true;
+        assertEquals(tankIsMoving, tankExpectedIsMoving);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testTankIsNotMovingOnMovementAndUpdateState(Direction direction) {
+        float movementSpeed = 1f;
+        Tank tank = new Tank(new Coordinates(0, 0), Direction.RIGHT, movementSpeed);
+        tank.startMovement(direction);
+        float deltaTime = 1 / movementSpeed;
+        tank.updateState(deltaTime);
+        boolean tankIsMoving = tank.isMoving();
+        boolean tankExpectedIsMoving = false;
+        assertEquals(tankIsMoving, tankExpectedIsMoving);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testTankCoordinatesOnMovementAndUpdateState(Direction direction) {
+        float movementSpeed = 1f;
+        Tank tank = new Tank(new Coordinates(0, 0), Direction.RIGHT, movementSpeed);
+        tank.startMovement(direction);
+        Coordinates tankExpectedCoordinates = direction.apply(tank.getCoordinates());
+        float deltaTime = 1 / movementSpeed;
+        tank.updateState(deltaTime);
+        Coordinates tankCoordinates = tank.getCoordinates();
+        assertEquals(tankCoordinates, tankExpectedCoordinates);
+    }
 
     @Test
     void testTankMovement() {
@@ -32,10 +90,8 @@ class TankTest {
         assertEquals(controllable, new Tank(new Coordinates(-1, 0), Direction.RIGHT, 1f));
         controllable = tank.afterAction(MoveAction.UP);
         assertEquals(controllable, new Tank(new Coordinates(0, 1), Direction.RIGHT, 1f));
-        controllable = tank.afterAction(null);
-        assertNull(controllable);
         tank.startMovement(Direction.RIGHT);
         controllable = tank.afterAction(MoveAction.UP);
-        assertNull(controllable);
+        assertEquals(controllable, tank);
     }
 }
